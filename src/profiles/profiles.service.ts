7c9c0df339358e76ffffe4843ba2212b0e4a6efe
Sebastian Sala <sebastian_orko@hotmail.com>
2024-07-28 17:13:40 -0600

    refactor: Update ProfilesService to use TypeORM for profile updates

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,11 +1,9 @@
-import { Injectable } from '@nestjs/common';
-
-import { UpdateProfileDto } from './dto/update-profile.dto';
-import { User } from 'src/users/entities/user.entity';
-import { CreateProfileDto } from './dto/create-profile.dto';
-import { Repository } from 'typeorm';
-import { Profile } from './entities/profile.entity';
+import { EntityNotFoundError, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { Profile } from './entities/profile.entity';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
 export class ProfilesService {
@@ -14,23 +12,44 @@ export class ProfilesService {
     private readonly profilesRepository: Repository<Profile>,
   ) {}
 
-  async create(
-    user: User,
-    createProfileDto: CreateProfileDto,
-  ): Promise<Profile> {
-    const profile = this.profilesRepository.create({
-      ...createProfileDto,
-      user,
+  async update(userId: string, updateProfileDto: UpdateProfileDto) {
+    // Find the profile by user ID
+    const profile = await this.findOneBy({
+      user: { id: userId },
     });
 
-    return await this.profilesRepository.save(profile);
+    // Prepare the updated DTO
+    const updatedDto = this.prepareUpdateDto(updateProfileDto);
+
+    // Update the profile
+    await this.profilesRepository.update(profile.id, updatedDto);
+
+    // Return the updated profile
+    return await this.findOneBy({ id: profile.id });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} profile`;
+  // Convert a string date to a Date object
+  private convertDate(date?: string): Date | undefined {
+    return date ? new Date(date) : undefined;
   }
 
-  update(id: number, updateProfileDto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
+  // Prepare the updated DTO
+  private prepareUpdateDto(updateProfileDto: UpdateProfileDto) {
+    const { birthdate, ...rest } = updateProfileDto;
+    return {
+      ...rest,
+      birthdate: this.convertDate(birthdate),
+    };
+  }
+
+  // Find a profile by options
+  private async findOneBy(options: any) {
+    const profile = await this.profilesRepository.findOne(options);
+
+    if (!profile) {
+      throw new NotFoundException('Profile not found');
+    }
+
+    return profile;
   }
 }
