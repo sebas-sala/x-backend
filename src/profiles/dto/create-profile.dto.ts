c176eec21093674cd82692bc2484c4e4385ec1ec
Sebastian Sala <sebastian_orko@hotmail.com>
2024-07-28 00:08:36 -0600

    refactor: Update CreateProfileDto with optional fields and validation

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProfileDto {}
+// create-profile.dto.ts
+
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateProfileDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  bio?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  location?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(20)
+  birthdate?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  website?: string;
+
+  // @IsOptional()
+  // @IsString()
+  // avatarUrl?: string;
+}
