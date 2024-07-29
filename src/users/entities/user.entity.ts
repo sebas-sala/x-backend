f01ba050ad621e83a546f28d2e90899c190f7dbc
Sebastian Sala <sebastian_orko@hotmail.com>
2024-07-28 19:30:43 -0600

    refactor: Update User entity to include posts relationship

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,19 +3,19 @@ import {
   Column,
   Entity,
   OneToOne,
-  OneToMany,
   JoinColumn,
   UpdateDateColumn,
   CreateDateColumn,
   PrimaryGeneratedColumn,
+  OneToMany,
 } from 'typeorm';
 
-// import { Post } from 'src/posts/entities/post.entity';
-// import { Like } from 'src/likes/entities/like.entity';
-// import { Follow } from 'src/follows/entities/follow.entity';
-// import { Comment } from 'src/comments/entities/comment.entity';
+import { Post } from 'src/posts/entities/post.entity';
+import { Like } from 'src/likes/entities/like.entity';
+import { Follow } from 'src/follows/entities/follow.entity';
+import { Comment } from 'src/comments/entities/comment.entity';
 import { Profile } from 'src/profiles/entities/profile.entity';
-// import { Bookmark } from 'src/bookmarks/entities/bookmark.entity';
+import { Bookmark } from 'src/bookmarks/entities/bookmark.entity';
 
 @Entity()
 export class User {
@@ -52,8 +52,8 @@ export class User {
   @JoinColumn({ name: 'profileId' })
   profile: Profile;
 
-  // @OneToMany(() => Post, (post) => post.user)
-  // posts: Post[];
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 
   // @OneToMany(() => Bookmark, (bookmark) => bookmark.user)
   // bookmarks: Bookmark[];
