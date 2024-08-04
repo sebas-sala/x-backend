98de7f46e81b2d2e0f02c488a1e48c7dc49496bb
Sebastian Sala <sebastian_orko@hotmail.com>
2024-08-04 14:25:34 -0600

    refactor: Remove unused code related to likes, images, bookmarks, and their modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,28 +7,15 @@ import { ThrottlerModule, seconds } from '@nestjs/throttler';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
-import { User } from './users/entities/user.entity';
-import { Post } from './posts/entities/post.entity';
-import { Like } from './likes/entities/like.entity';
-import { Image } from './images/entities/image.entity';
-import { Follow } from './follows/entities/follow.entity';
-import { Comment } from './comments/entities/comment.entity';
-import { Profile } from './profiles/entities/profile.entity';
-import { Bookmark } from './bookmarks/entities/bookmark.entity';
-
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { LikesModule } from './likes/likes.module';
 import { FollowModule } from './follows/follows.module';
-import { ImagesModule } from './images/images.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { CommentsModule } from './comments/comments.module';
-import { BookmarksModule } from './bookmarks/bookmarks.module';
 
 import configuration from './config/configuration';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { PostsModule } from './posts/posts.module';
-import { QueryRunnerFactory } from './common/factories/query-runner.factory';
 
 @Module({
   imports: [
@@ -51,12 +38,9 @@ import { QueryRunnerFactory } from './common/factories/query-runner.factory';
     AuthModule,
     UsersModule,
     PostsModule,
-    LikesModule,
     FollowModule,
-    ImagesModule,
     ProfilesModule,
     CommentsModule,
-    BookmarksModule,
   ],
   controllers: [AppController],
   providers: [
