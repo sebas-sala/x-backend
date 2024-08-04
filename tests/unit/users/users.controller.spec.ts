bddc34e2d97da7e080e44b885d194f2605f47c67
Sebastian Sala <sebastian_orko@hotmail.com>
2024-08-03 18:33:26 -0600

    refactor: Update UserFactory and ProfileFactory to use class methods and improve code structure

--- tests/unit/users/users.controller.spec.ts
+++ tests/unit/users/users.controller.spec.ts
@@ -5,77 +5,49 @@ import { ConflictException, NotFoundException } from '@nestjs/common';
 import { User } from '@/src/users/entities/user.entity';
 import { UsersService } from '@/src/users/users.service';
 import { UsersController } from '@/src/users/users.controller';
-import { CreateUserDto } from '@/src/users/dto/create-user.dto';
 
 import { Profile } from '@/src/profiles/entities/profile.entity';
-import { ProfilesModule } from '@/src/profiles/profiles.module';
 import { ProfilesService } from '@/src/profiles/profiles.service';
 
 import { Post } from '@/src/posts/entities/post.entity';
 
-import { QueryRunnerFactory } from '@/src/common/factories/query-runner.factory';
-import { createMockQueryRunner } from '@/tests/utils/mocks/query-runner.mock';
-
-const mockUser: User = {
-  name: 'User 1',
-  email: 'HlqQp@example.com',
-  username: 'user1',
-  password: 'password1',
-  id: '1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  profile: undefined as any,
-  posts: [],
-};
-
-const mockProfile: Profile = {
-  id: '1',
-  bio: 'test bio',
-  birthdate: new Date(),
-  isPublic: false,
-  location: 'test location',
-  website: 'test website',
-  updatedAt: new Date(),
-  user: {} as any,
-};
+import UserFactory from '@/tests/utils/factories/user.factory';
+import ProfileFactory from '@/tests/utils/factories/profile.factory';
 
 describe('UsersController', () => {
   let usersController: UsersController;
   let usersService: UsersService;
+
   let profilesService: ProfilesService;
-  let queryRunner: any;
 
   beforeEach(async () => {
-    queryRunner = createMockQueryRunner();
-
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        ProfilesModule,
         TypeOrmModule.forRoot({
           type: 'sqlite',
           database: ':memory:',
           entities: [User, Profile, Post],
           synchronize: true,
         }),
-        TypeOrmModule.forFeature([User]),
+        TypeOrmModule.forFeature([User, Profile]),
       ],
       controllers: [UsersController],
       providers: [
-        UsersService,
-        ProfilesService,
         {
-          provide: QueryRunnerFactory,
+          provide: UsersService,
           useValue: {
-            createQueryRunner: jest.fn(() => queryRunner),
+            findAll: jest.fn(),
+            findOneById: jest.fn(),
+            findByUsername: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
           },
         },
         {
           provide: ProfilesService,
           useValue: {
+            update: jest.fn(),
             findOneBy: jest.fn(),
-            update: jest
-              .fn()
-              .mockImplementation(() => Promise.resolve(mockProfile)),
           },
         },
       ],
@@ -88,28 +60,39 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(usersController).toBeDefined();
+    expect(usersService).toBeDefined();
+    expect(profilesService).toBeDefined();
   });
 
   describe('findAll()', () => {
+    const mockUser = UserFactory.createUserData() as User;
+
     it('should return an array of users', async () => {
       jest.spyOn(usersService, 'findAll').mockResolvedValueOnce([mockUser]);
+
       const result = await usersController.findAll();
+
       expect(result).toEqual([mockUser]);
       expect(usersService.findAll).toHaveBeenCalled();
-      expect(usersService.findAll).toHaveBeenCalledTimes(1);
     });
 
     it('should return an empty array if there are no users', async () => {
       jest.spyOn(usersService, 'findAll').mockResolvedValueOnce([]);
+
       const result = await usersController.findAll();
+
       expect(result).toEqual([]);
     });
   });
 
   describe('findOne()', () => {
+    const mockUser = UserFactory.createUserData() as User;
+
     it('should return a user', async () => {
       jest.spyOn(usersService, 'findOneById').mockResolvedValueOnce(mockUser);
+
       const result = await usersController.findOne('1');
+
       expect(result).toEqual(mockUser);
       expect(usersService.findOneById).toHaveBeenCalledWith('1');
     });
@@ -122,7 +105,6 @@ describe('UsersController', () => {
       await expect(usersController.findOne('1')).rejects.toThrow(
         NotFoundException,
       );
-
       await expect(usersController.findOne('1')).rejects.toThrow(
         'User not found',
       );
@@ -130,22 +112,27 @@ describe('UsersController', () => {
   });
 
   describe('getProfile()', () => {
+    const mockUser = UserFactory.createUserData() as User;
+
     it('should return a user profile', async () => {
-      jest.spyOn(usersService, 'findOneById').mockResolvedValueOnce(mockUser);
+      jest
+        .spyOn(usersService, 'findByUsername')
+        .mockResolvedValueOnce(mockUser);
+
       const result = await usersController.getProfile('1');
+
       expect(result).toEqual(mockUser);
-      expect(usersService.findOneById).toHaveBeenCalledWith('1');
+      expect(usersService.findByUsername).toHaveBeenCalledWith('1');
     });
 
     it('should throw an error if the user does not exist', async () => {
       jest
-        .spyOn(usersService, 'findOneById')
+        .spyOn(usersService, 'findByUsername')
         .mockRejectedValue(new NotFoundException('User not found'));
 
       await expect(usersController.getProfile('1')).rejects.toThrow(
         NotFoundException,
       );
-
       await expect(usersController.getProfile('1')).rejects.toThrow(
         'User not found',
       );
@@ -153,83 +140,85 @@ describe('UsersController', () => {
   });
 
   describe('create()', () => {
-    const createUserDto: CreateUserDto = {
-      name: 'User 3',
-      email: 'newEmail@gmail.com',
-      username: 'user3',
-      password: 'password3',
-    };
+    const mockUser = UserFactory.createUserData() as User;
+
+    const createUserDto = UserFactory.createUserDto();
 
     it('should create a user', async () => {
-      const { manager } = queryRunner;
-
-      jest.spyOn(manager, 'create').mockResolvedValueOnce({
-        ...createUserDto,
-        id: '3',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      });
+      jest.spyOn(usersService, 'create').mockResolvedValueOnce(mockUser);
 
       const result = await usersController.create(createUserDto);
 
-      expect(result).toEqual({
-        ...createUserDto,
-        createdAt: expect.any(Date),
-        updatedAt: expect.any(Date),
-        password: expect.any(String),
-        id: '3',
-      });
-      expect(manager.create).toHaveBeenCalled();
-      expect(manager.save).toHaveBeenCalled();
-      expect(manager.findOne).toHaveBeenCalledWith(User, {
-        where: [
-          { username: createUserDto.username },
-          { email: createUserDto.email },
-        ],
-      });
+      expect(result).toEqual(mockUser);
     });
 
     it('should throw an error if the username already exists', async () => {
-      const { manager } = queryRunner;
+      jest
+        .spyOn(usersService, 'create')
+        .mockRejectedValue(new ConflictException('Username already exists'));
 
-      jest.spyOn(manager, 'findOne').mockResolvedValue(mockUser);
-
-      await expect(usersController.create(mockUser)).rejects.toThrow(
+      await expect(usersController.create(mockUser)).rejects.toBeInstanceOf(
         ConflictException,
       );
-      await expect(usersController.create(mockUser)).rejects.toThrow(
+      await expect(usersController.create(mockUser)).rejects.toHaveProperty(
+        'message',
         'Username already exists',
       );
     });
 
     it('should throw an error if the email already exists', async () => {
-      const { manager } = queryRunner;
+      jest
+        .spyOn(usersService, 'create')
+        .mockRejectedValue(new ConflictException('Email already exists'));
 
-      jest.spyOn(manager, 'findOne').mockResolvedValue({
-        ...mockUser,
-        username: 'aoierstnoart',
-      });
-
-      await expect(usersController.create(mockUser)).rejects.toThrow(
+      await expect(usersController.create(mockUser)).rejects.toBeInstanceOf(
         ConflictException,
       );
-      await expect(usersController.create(mockUser)).rejects.toThrow(
+      await expect(usersController.create(mockUser)).rejects.toHaveProperty(
+        'message',
         'Email already exists',
       );
     });
   });
 
   describe('updateProfile()', () => {
-    it('should update a user profile', async () => {
-      const updateProfileDto = {
-        bio: 'New bio',
-        birthdate: '2020-01-01',
-      };
+    const mockProfile = ProfileFactory.createProfileData() as Profile;
 
+    const updateProfileDto = {
+      bio: 'New bio',
+      birthdate: '2020-01-01',
+    };
+
+    it('should update a user profile', async () => {
       jest.spyOn(profilesService, 'update').mockResolvedValueOnce(mockProfile);
 
-      const result = await usersController.updateProfile('1', updateProfileDto);
+      const result = await usersController.updateProfile(
+        mockProfile.id,
+        updateProfileDto,
+      );
+
       expect(result).toEqual(mockProfile);
+      expect(profilesService.update).toHaveBeenCalledWith(
+        mockProfile.id,
+        updateProfileDto,
+      );
+    });
+
+    it('should throw an error if the profile does not exist', async () => {
+      jest
+        .spyOn(profilesService, 'update')
+        .mockRejectedValue(new NotFoundException('Profile not found'));
+
+      await expect(
+        usersController.updateProfile('1', updateProfileDto),
+      ).rejects.toThrow(NotFoundException);
+      await expect(
+        usersController.updateProfile('1', updateProfileDto),
+      ).rejects.toThrow('Profile not found');
+      expect(profilesService.update).toHaveBeenCalledWith(
+        '1',
+        updateProfileDto,
+      );
     });
   });
 });
