7f543cbe4d4006c0d72cfe8a828bdd552ddeac24
Sebastian Sala <sebastian_orko@hotmail.com>
2024-08-04 22:41:34 -0600

    refactor: Add Swagger documentation for the getHello() endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Get hello' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return hello',
+    example: 'Hello World!',
+  })
   @Get()
   getHello(): string {
     return this.appService.getHello();
