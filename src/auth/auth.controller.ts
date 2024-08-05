086a3731bdf3c609fdd9490294bc4a603eb750e2
Sebastian Sala <sebastian_orko@hotmail.com>
2024-08-04 22:41:45 -0600

    refactor: Add login endpoint with Swagger documentation and validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,17 +6,32 @@ import {
   UseGuards,
   Request,
 } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 import { LocalStrategy } from './local.strategy';
-import { ApiTags } from '@nestjs/swagger';
+
+import { LoginResponseSchema } from '../swagger/schemas/auth';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({
+    status: 200,
+    type: LoginResponseSchema,
+    description: 'Return token',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    example: 'Invalid credentials',
+  })
+  @ApiBody({ type: LoginAuthDto })
   @UseGuards(LocalStrategy)
   @HttpCode(HttpStatus.OK)
   @Post('login')
