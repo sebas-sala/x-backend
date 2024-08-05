da58e39be53da3a6af86b4381f68677629231bc1
Sebastian Sala <sebastian_orko@hotmail.com>
2024-08-04 22:41:39 -0600

    refactor: Update SwaggerModule setup path to '/swagger'

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/swagger', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
