0ad06743917ab3f4c1dfc4d861f4d3e83a92787c
sebas-sala <sebastian_orko@hotmail.com>
2024-08-29 08:32:25 -0600

    refactor: Update BlockedUsersModule to import User entity and QueryRunnerFactory

--- src/blocked-users/blocked-users.module.ts
+++ src/blocked-users/blocked-users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { BlockedUsersService } from './blocked-users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlockedUser } from './entities/blocked-user.entity';
+import { User } from '../users/entities/user.entity';
+import { QueryRunnerFactory } from '../common/factories/query-runner.factory';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BlockedUser])],
-  controllers: [],
-  providers: [BlockedUsersService],
+  imports: [TypeOrmModule.forFeature([BlockedUser, User])],
+  providers: [BlockedUsersService, QueryRunnerFactory],
   exports: [BlockedUsersService],
 })
 export class BlockedUsersModule {}
