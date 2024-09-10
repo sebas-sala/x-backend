325b26a66ba85840d5e9523dac9e3b5bfd536374
sebas-sala <sebastian_orko@hotmail.com>
2024-09-09 22:58:04 -0600

    feat: Add CommentsController to handle comment creation and replies

---
+++ src/comments/comments.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
+import { CreateCommentDto } from './dto/create-comment.dto';
+
+@ApiTags('comments')
+@Controller('comments')
+export class CommentsController {
+  constructor(private readonly commentsService: CommentsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':id/replies')
+  createReply(
+    @Param('id') id: string,
+    @Body() createCommentDto: CreateCommentDto,
+    @CurrentUser() currentUser: string,
+  ) {
+    console.log('id', currentUser);
+    return this.commentsService.createCommentReply(
+      id,
+      createCommentDto,
+      currentUser,
+    );
+  }
+}
--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,10 +3,12 @@ import { CommentsService } from './comments.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
 import { Post } from '../posts/entities/post.entity';
+import { CommentsController } from './comments.controller';
 
 @Module({
   providers: [CommentsService],
   imports: [TypeOrmModule.forFeature([Comment, Post])],
   exports: [CommentsService],
+  controllers: [CommentsController],
 })
 export class CommentsModule {}
