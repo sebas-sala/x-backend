eeafb4f7f78bf5d3a4f5eac4981b293a8b581e98
sebas-sala <sebastian_orko@hotmail.com>
2024-09-22 15:26:37 -0600

    feat: Add factory modules for testing purposes

---
+++ tests/utils/factories/index.ts
@@ -0,0 +1,11 @@
+// factories/index.ts
+
+export { default as AuthFactory } from './auth.factory';
+export { default as PostFactory } from './post.factory';
+export { default as UserFactory } from './user.factory';
+export { default as LikeFactory } from './like.factory';
+export { default as FollowFactory } from './follow.factory';
+export { default as MessageFactory } from './message.factory';
+export { default as ProfileFactory } from './profile.factory';
+export { default as CommentFactory } from './comment.factory';
+export { default as BlockedUserFactory } from './blocked-user.factory';
