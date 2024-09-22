7f28dc75222f45fb0a5fa81cadeb39fa47719207
sebas-sala <sebastian_orko@hotmail.com>
2024-09-22 17:47:07 -0600

    refactor: Remove unused AllExceptionsFilter class

--- tests/e2e/posts.e2e-spec.ts
+++ tests/e2e/posts.e2e-spec.ts
@@ -1,35 +1,19 @@
-import { Test } from '@nestjs/testing';
 import { DataSource } from 'typeorm';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { JwtService } from '@nestjs/jwt';
 
 import { User } from '@/src/users/entities/user.entity';
-import { UsersModule } from '@/src/users/users.module';
+import { Like } from '@/src/likes/entities/like.entity';
+import { Post } from '@/src/posts/entities/post.entity';
+import { Comment } from '@/src/comments/entities/comment.entity';
+import { BlockedUser } from '@/src/blocked-users/entities/blocked-user.entity';
 
 import UserFactory from '../utils/factories/user.factory';
-
-import { Profile } from '@/src/profiles/entities/profile.entity';
-
-import { ValidationPipe } from '@nestjs/common';
-import { Follow } from '@/src/follows/entities/follow.entity';
-
-import { AuthModule } from '@/src/auth/auth.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { AuthService } from '@/src/auth/auth.service';
-import { JwtStrategy } from '@/src/auth/jwt.strategy';
-
-import { BlockedUser } from '@/src/blocked-users/entities/blocked-user.entity';
 import PostFactory from '../utils/factories/post.factory';
-import { Post } from '@/src/posts/entities/post.entity';
-import { PostsModule } from '@/src/posts/posts.module';
-import { Comment } from '@/src/comments/entities/comment.entity';
-import CommentFactory from '../utils/factories/comment.factory';
-import { Like } from '@/src/likes/entities/like.entity';
-import { LikesModule } from '@/src/likes/likes.module';
 import LikeFactory from '../utils/factories/like.factory';
+import CommentFactory from '../utils/factories/comment.factory';
+
+import { setupTestApp } from '../utils/setup-test-app';
 
 describe('Posts API (e2e)', () => {
   let app: NestFastifyApplication;
@@ -42,71 +26,30 @@ describe('Posts API (e2e)', () => {
 
   let userFactory: UserFactory;
   let postFactory: PostFactory;
-  let commentFactory: CommentFactory;
   let likeFactory: LikeFactory;
+  let commentFactory: CommentFactory;
 
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [
-        UsersModule,
-        AuthModule,
-        PostsModule,
-        LikesModule,
-        TypeOrmModule.forRoot({
-          type: 'sqlite',
-          database: ':memory:',
-          autoLoadEntities: true,
-          synchronize: true,
-        }),
-        TypeOrmModule.forFeature([
-          User,
-          Follow,
-          Profile,
-          BlockedUser,
-          Post,
-          Comment,
-          Like,
-        ]),
+    const setup = await setupTestApp();
 
-        JwtModule.register({
-          secret: process.env.JWT_SECRET || 'secret',
-          signOptions: { expiresIn: '60m' },
-        }),
-      ],
-      providers: [AuthService, JwtStrategy],
-    }).compile();
+    app = setup.app;
+    dataSource = setup.dataSource;
+    jwtService = setup.jwtService;
 
-    app = moduleRef.createNestApplication<NestFastifyApplication>(
-      new FastifyAdapter(),
-    );
-    app.useGlobalPipes(new ValidationPipe());
-
-    await app.init();
-    await app.getHttpAdapter().getInstance().ready();
-
-    dataSource = moduleRef.get<DataSource>(DataSource);
-    jwtService = moduleRef.get<JwtService>(JwtService);
-
-    userFactory = new UserFactory(dataSource);
-    postFactory = new PostFactory(dataSource);
-    commentFactory = new CommentFactory(dataSource);
-    likeFactory = new LikeFactory(dataSource);
+    userFactory = setup.userFactory;
+    postFactory = setup.postFactory;
+    likeFactory = setup.likeFactory;
+    commentFactory = setup.commentFactory;
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  beforeAll(async () => {
-    await dataSource.query('PRAGMA foreign_keys = ON');
-  });
-
   beforeEach(async () => {
     await dataSource.synchronize(true);
 
-    currentUser = await dataSource
-      .getRepository(User)
-      .save(UserFactory.createUserDto());
+    currentUser = await userFactory.createUserEntity();
 
     token = jwtService.sign({ sub: currentUser.id });
   });
@@ -201,6 +144,24 @@ describe('Posts API (e2e)', () => {
       expect(response.statusCode).toBe(201);
       expect(JSON.parse(response.payload)).toMatchObject(post);
     });
+
+    it('should return a 401 if the user is not authenticated', async () => {
+      const post = {
+        content: 'This is a post',
+      };
+
+      const response = await app.inject({
+        method: 'POST',
+        url: '/posts',
+        payload: post,
+      });
+
+      expect(response.statusCode).toBe(401);
+      expect(JSON.parse(response.payload)).toMatchObject({
+        message: 'Unauthorized',
+        statusCode: 401,
+      });
+    });
   });
 
   describe('GET /posts/:id', () => {
@@ -235,47 +196,62 @@ describe('Posts API (e2e)', () => {
       expect(response.statusCode).toBe(404);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Post not found',
-        error: 'Not Found',
+        error: 'NotFoundException',
         statusCode: 404,
       });
     });
   });
 
-  describe('PATCH /posts/:id', () => {
-    it('should update a post', async () => {
+  describe('GET /posts/:id/comments', () => {
+    it('should return a list of comments', async () => {
       const post = await postFactory.createPostEntity({
         userId: currentUser.id,
       });
 
-      const updatedPost = PostFactory.createPostDto();
+      const comments = await Promise.all([
+        commentFactory.createPostCommentEntity({
+          postId: post.id,
+          userId: currentUser.id,
+        }),
+        commentFactory.createPostCommentEntity({
+          postId: post.id,
+          userId: currentUser.id,
+        }),
+        commentFactory.createPostCommentEntity({
+          postId: post.id,
+          userId: currentUser.id,
+        }),
+      ]);
 
       const response = await app.inject({
-        method: 'PATCH',
-        url: `/posts/${post.id}`,
+        method: 'GET',
+        url: `/posts/${post.id}/comments`,
         headers: {
           Authorization: `Bearer ${token}`,
         },
-        payload: updatedPost,
       });
 
       expect(response.statusCode).toBe(200);
-      expect(JSON.parse(response.payload)).toMatchObject(updatedPost);
+      expect(
+        JSON.parse(response.payload)
+          .map((comment: Comment) => comment.id)
+          .sort(),
+      ).toEqual(comments.map((comment) => comment.id).sort());
     });
 
     it('should return a 404 if the post does not exist', async () => {
       const response = await app.inject({
-        method: 'PATCH',
-        url: '/posts/1',
+        method: 'GET',
+        url: '/posts/1/comments',
         headers: {
           Authorization: `Bearer ${token}`,
         },
-        payload: PostFactory.createPostDto(),
       });
 
       expect(response.statusCode).toBe(404);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Post not found',
-        error: 'Not Found',
+        error: 'NotFoundException',
         statusCode: 404,
       });
     });
@@ -339,63 +315,118 @@ describe('Posts API (e2e)', () => {
       expect(response.statusCode).toBe(404);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Post not found',
-        error: 'Not Found',
+        error: 'NotFoundException',
         statusCode: 404,
       });
     });
   });
 
-  describe('GET /posts/:id/comments', () => {
-    it('should return a list of comments', async () => {
+  describe('PATCH /posts/:id', () => {
+    it('should update a post', async () => {
       const post = await postFactory.createPostEntity({
         userId: currentUser.id,
       });
 
-      const comments = await Promise.all([
-        commentFactory.createPostCommentEntity({
-          postId: post.id,
-          userId: currentUser.id,
-        }),
-        commentFactory.createPostCommentEntity({
-          postId: post.id,
-          userId: currentUser.id,
-        }),
-        commentFactory.createPostCommentEntity({
-          postId: post.id,
-          userId: currentUser.id,
-        }),
-      ]);
+      const updatedPost = PostFactory.createPostDto();
 
       const response = await app.inject({
-        method: 'GET',
-        url: `/posts/${post.id}/comments`,
+        method: 'PATCH',
+        url: `/posts/${post.id}`,
         headers: {
           Authorization: `Bearer ${token}`,
         },
+        payload: updatedPost,
       });
 
       expect(response.statusCode).toBe(200);
-      expect(
-        JSON.parse(response.payload)
-          .map((comment: Comment) => comment.id)
-          .sort(),
-      ).toEqual(comments.map((comment) => comment.id).sort());
+      expect(JSON.parse(response.payload)).toMatchObject(updatedPost);
+    });
+
+    it('should return a 401 if the user is not authenticated', async () => {
+      const post = await postFactory.createPostEntity({
+        userId: currentUser.id,
+      });
+
+      const response = await app.inject({
+        method: 'PATCH',
+        url: `/posts/${post.id}`,
+        payload: PostFactory.createPostDto(),
+      });
+
+      expect(response.statusCode).toBe(401);
+      expect(JSON.parse(response.payload)).toMatchObject({
+        message: 'Unauthorized',
+        statusCode: 401,
+      });
     });
 
     it('should return a 404 if the post does not exist', async () => {
       const response = await app.inject({
-        method: 'GET',
-        url: '/posts/1/comments',
+        method: 'PATCH',
+        url: '/posts/1',
         headers: {
           Authorization: `Bearer ${token}`,
         },
+        payload: PostFactory.createPostDto(),
       });
 
       expect(response.statusCode).toBe(404);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Post not found',
-        error: 'Not Found',
+        error: 'NotFoundException',
         statusCode: 404,
+      });
+    });
+
+    describe('DELETE /posts/:id', () => {
+      it('should delete a post', async () => {
+        const post = await postFactory.createPostEntity({
+          userId: currentUser.id,
+        });
+
+        const response = await app.inject({
+          method: 'DELETE',
+          url: `/posts/${post.id}`,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        expect(response.statusCode).toBe(200);
+      });
+
+      it('should return a 401 if the user is not authenticated', async () => {
+        const post = await postFactory.createPostEntity({
+          userId: currentUser.id,
+        });
+
+        const response = await app.inject({
+          method: 'DELETE',
+          url: `/posts/${post.id}`,
+        });
+
+        expect(response.statusCode).toBe(401);
+        expect(JSON.parse(response.payload)).toMatchObject({
+          message: 'Unauthorized',
+          statusCode: 401,
+        });
+      });
+
+      it('should return a 404 if the post does not exist', async () => {
+        const response = await app.inject({
+          method: 'DELETE',
+          url: '/posts/1',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        expect(response.statusCode).toBe(404);
+        expect(JSON.parse(response.payload)).toMatchObject({
+          message: 'Post not found',
+          error: 'NotFoundException',
+          statusCode: 404,
+        });
       });
     });
   });
@@ -406,11 +437,7 @@ describe('Posts API (e2e)', () => {
         userId: currentUser.id,
       });
 
-      const likes = await Promise.all([
-        likeFactory.createPostLike(post.id, currentUser.id),
-        likeFactory.createPostLike(post.id, currentUser.id),
-        likeFactory.createPostLike(post.id, currentUser.id),
-      ]);
+      const likes = await likeFactory.createPostLike(post.id, currentUser.id);
 
       const response = await app.inject({
         method: 'GET',
@@ -425,7 +452,7 @@ describe('Posts API (e2e)', () => {
         JSON.parse(response.payload)
           .map((like: Like) => like.id)
           .sort(),
-      ).toEqual(likes.map((like) => like.id).sort());
+      ).toEqual([likes.id]);
     });
   });
 
@@ -450,6 +477,23 @@ describe('Posts API (e2e)', () => {
       });
     });
 
+    it('should return a 401 if the user is not authenticated', async () => {
+      const post = await postFactory.createPostEntity({
+        userId: currentUser.id,
+      });
+
+      const response = await app.inject({
+        method: 'POST',
+        url: `/posts/${post.id}/likes`,
+      });
+
+      expect(response.statusCode).toBe(401);
+      expect(JSON.parse(response.payload)).toMatchObject({
+        message: 'Unauthorized',
+        statusCode: 401,
+      });
+    });
+
     it('should return a 404 if the post does not exist', async () => {
       const response = await app.inject({
         method: 'POST',
@@ -462,7 +506,7 @@ describe('Posts API (e2e)', () => {
       expect(response.statusCode).toBe(404);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Post not found by id ' + 1,
-        error: 'Not Found',
+        error: 'NotFoundException',
         statusCode: 404,
       });
     });
@@ -485,7 +529,7 @@ describe('Posts API (e2e)', () => {
       expect(response.statusCode).toBe(409);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Like already exists',
-        error: 'Conflict',
+        error: 'ConflictException',
         statusCode: 409,
       });
     });
@@ -497,7 +541,7 @@ describe('Posts API (e2e)', () => {
         userId: currentUser.id,
       });
 
-      const like = await likeFactory.createPostLike(post.id, currentUser.id);
+      await likeFactory.createPostLike(post.id, currentUser.id);
 
       const response = await app.inject({
         method: 'DELETE',
@@ -508,6 +552,23 @@ describe('Posts API (e2e)', () => {
       });
 
       expect(response.statusCode).toBe(200);
+    });
+
+    it('should return a 401 if the user is not authenticated', async () => {
+      const post = await postFactory.createPostEntity({
+        userId: currentUser.id,
+      });
+
+      const response = await app.inject({
+        method: 'DELETE',
+        url: `/posts/${post.id}/likes`,
+      });
+
+      expect(response.statusCode).toBe(401);
+      expect(JSON.parse(response.payload)).toMatchObject({
+        message: 'Unauthorized',
+        statusCode: 401,
+      });
     });
 
     it('should return a 404 if the post does not exist', async () => {
@@ -522,7 +583,7 @@ describe('Posts API (e2e)', () => {
       expect(response.statusCode).toBe(404);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Post not found by id ' + 1,
-        error: 'Not Found',
+        error: 'NotFoundException',
         statusCode: 404,
       });
     });
@@ -543,7 +604,7 @@ describe('Posts API (e2e)', () => {
       expect(response.statusCode).toBe(404);
       expect(JSON.parse(response.payload)).toMatchObject({
         message: 'Like not found',
-        error: 'Not Found',
+        error: 'NotFoundException',
         statusCode: 404,
       });
     });
