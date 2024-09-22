eb02ed23f7dc7e7a33643e95fe94cca840883c99
sebas-sala <sebastian_orko@hotmail.com>
2024-09-22 17:47:50 -0600

    refactor: Add TypeOrmModule import for Message and User entities in MessagesModule

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Message } from './entities/message.entity';
 import { MessagesService } from './messages.service';
 import { MessagesGateway } from './messages.gateway';
 
+import { User } from '../users/entities/user.entity';
+
 @Module({
   providers: [MessagesGateway, MessagesService],
+  imports: [TypeOrmModule.forFeature([Message, User])],
 })
 export class MessagesModule {}
