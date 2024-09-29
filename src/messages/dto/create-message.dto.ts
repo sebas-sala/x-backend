b9e75cdbe4d825f4ec8806b94fe1154e6151ea04
sebas-sala <sebastian_orko@hotmail.com>
2024-09-29 13:06:27 -0600

    refactor: Update CreateMessageDto validation decorators

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
   receiver: string;
+
+  @IsString()
+  @IsNotEmpty()
   content: string;
 }
