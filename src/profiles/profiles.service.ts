401ce102b22475eea17beb9a2fa3cfedd3b73502
sebas-sala <sebastian_orko@hotmail.com>
2024-09-29 13:01:45 -0600

    refactor: Update ProfilesService to handle profile updates and creation

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,8 +1,14 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Injectable, NotFoundException } from '@nestjs/common';
 
+import { User } from '../users/entities/user.entity';
 import { Profile } from './entities/profile.entity';
+
 import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
@@ -12,22 +18,41 @@ export class ProfilesService {
     private readonly profilesRepository: Repository<Profile>,
   ) {}
 
-  async update(userId: string, updateProfileDto: UpdateProfileDto) {
-    const profile = await this.findOneBy({
-      user: { id: userId },
-    });
+  async update(
+    userId: string,
+    updateProfileDto: UpdateProfileDto,
+    currentUser: User,
+  ) {
+    this.validateProfileOwner(userId, currentUser.id);
 
-    const updatedDto = this.prepareUpdateDto(updateProfileDto);
-    await this.profilesRepository.update(profile.id, updatedDto);
+    let profile = await this.findProfileByUserId(userId);
 
-    return await this.findOneBy({ id: profile.id });
+    if (profile === null) {
+      profile = await this.createProfile(currentUser, updateProfileDto);
+    } else {
+      this.updateProfile(profile.id, updateProfileDto);
+    }
+
+    return await this.findProfileOrFail(profile.id);
   }
 
-  async findOneBy(options: any, relations?: string[]): Promise<Profile> {
+  async findProfileByUserId(userId: string): Promise<Profile | null> {
+    return await this.profilesRepository.findOneBy({
+      user: { id: userId },
+    });
+  }
+
+  async findOneBy(options: any, relations?: string[]): Promise<Profile | null> {
     const profile = await this.profilesRepository.findOne({
       where: options,
       relations,
     });
+
+    return profile;
+  }
+
+  async findOneByOrFail(options: any, relations?: string[]): Promise<Profile> {
+    const profile = await this.findOneBy(options, relations);
 
     if (!profile) {
       throw new NotFoundException('Profile not found');
@@ -36,9 +61,49 @@ export class ProfilesService {
     return profile;
   }
 
+  async findProfileOrFail(profileId: string): Promise<Profile> {
+    const profile = await this.profilesRepository.findOneBy({ id: profileId });
+
+    if (!profile) {
+      throw new NotFoundException('Profile not found');
+    }
+
+    return profile;
+  }
+
+  private async createProfile(
+    currentUser: User,
+    createProfileDto: UpdateProfileDto,
+  ) {
+    const profile = this.profilesRepository.create({
+      user: currentUser,
+      ...createProfileDto,
+    });
+
+    await this.profilesRepository.save(profile);
+
+    return await this.profilesRepository.findOneByOrFail({ id: profile.id });
+  }
+
+  private async updateProfile(
+    profileId: string,
+    updateProfileDto: UpdateProfileDto,
+  ): Promise<void> {
+    const updatedDto = this.prepareUpdateDto(updateProfileDto);
+    await this.profilesRepository.update(profileId, updatedDto);
+  }
+
   // Convert a string date to a Date object
   private convertDate(date?: string): Date | undefined {
     return date ? new Date(date) : undefined;
+  }
+
+  private validateProfileOwner(profileId: string, userId: string) {
+    if (profileId !== userId) {
+      throw new UnprocessableEntityException(
+        'You are not the owner of this profile',
+      );
+    }
   }
 
   // Prepare the updated DTO
