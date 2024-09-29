98c3acbd6531482b77b3f185d23ec7ac7d8d5f34
sebas-sala <sebastian_orko@hotmail.com>
2024-09-29 16:27:26 -0600

    refactor: Update LikesService in likes.service.ts

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,23 +1,25 @@
 import {
-  ConflictException,
   Injectable,
+  ConflictException,
   NotFoundException,
 } from '@nestjs/common';
-import { Like } from './entities/like.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Post } from '../posts/entities/post.entity';
-import { Comment } from '../comments/entities/comment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Like } from './entities/like.entity';
+import { User } from '../users/entities/user.entity';
+
+import { PostsService } from '../posts/posts.service';
+import { CommentsService } from '../comments/comments.service';
 
 @Injectable()
 export class LikesService {
   constructor(
     @InjectRepository(Like)
     private readonly likeRepository: Repository<Like>,
-    @InjectRepository(Post)
-    private readonly postRepository: Repository<Post>,
-    @InjectRepository(Comment)
-    private readonly commentRepository: Repository<Comment>,
+
+    private readonly postService: PostsService,
+    private readonly commentService: CommentsService,
   ) {}
 
   async getPostLikes(postId: string): Promise<Like[]> {
@@ -32,120 +34,84 @@ export class LikesService {
     });
   }
 
-  async likePost(postId: string, userId: string): Promise<Like> {
-    try {
-      await this.findPostById(postId);
-      await this.validateLikeDoesNotExists(postId, userId);
-      await this.findPostLike(postId, userId);
+  async likePost(postId: string, currentUser: User): Promise<Like> {
+    await this.postService.findPostByIdOrFail(postId);
+    await this.validateLikeDoesNotExists('post', postId, currentUser.id);
 
-      const like = this.likeRepository.create({
-        post: { id: postId },
-        user: { id: userId },
-      });
-
-      return await this.likeRepository.save(like);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async unlikePost(postId: string, userId: string): Promise<void> {
-    try {
-      await this.findPostById(postId);
-
-      const like = await this.likeRepository.findOne({
-        where: { post: { id: postId }, user: { id: userId } },
-      });
-
-      if (!like) {
-        throw new NotFoundException('Like not found');
-      }
-
-      await this.likeRepository.delete(like);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async likeComment(commentId: string, userId: string): Promise<Like> {
-    try {
-      await this.findCommentById(commentId);
-      await this.validateLikeDoesNotExists(userId, undefined, commentId);
-
-      const like = this.likeRepository.create({
-        comment: { id: commentId },
-        user: { id: userId },
-      });
-
-      return await this.likeRepository.save(like);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async unlikeComment(commentId: string, userId: string): Promise<void> {
-    try {
-      await this.findCommentById(commentId);
-
-      const like = await this.likeRepository.findOne({
-        where: { comment: { id: commentId }, user: { id: userId } },
-      });
-
-      if (!like) {
-        throw new NotFoundException('Like not found');
-      }
-
-      await this.likeRepository.delete(like);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  private async findCommentById(commentId: string): Promise<Comment> {
-    const comment = await this.commentRepository.findOneBy({
-      id: commentId,
+    const like = this.likeRepository.create({
+      post: { id: postId },
+      user: { id: currentUser.id },
     });
-
-    if (!comment) {
-      throw new NotFoundException('Comment not found by id ' + commentId);
-    }
-
-    return comment;
+    return await this.likeRepository.save(like);
   }
 
-  private async findPostById(postId: string): Promise<Post> {
-    const post = await this.postRepository.findOneBy({
-      id: postId,
+  async unlikePost(postId: string, currentUser: User): Promise<void> {
+    await this.postService.findPostByIdOrFail(postId);
+
+    const like = await this.findLikeByEntityAndUserOrFail(
+      'post',
+      postId,
+      currentUser.id,
+    );
+    await this.likeRepository.delete(like);
+  }
+
+  async likeComment(commentId: string, user: User): Promise<Like> {
+    await this.commentService.findCommentByIdOrFail(commentId);
+    await this.validateLikeDoesNotExists('comment', commentId, user.id);
+
+    const like = this.likeRepository.create({
+      comment: { id: commentId },
+      user: { id: user.id },
     });
-
-    if (!post) {
-      throw new NotFoundException('Post not found by id ' + postId);
-    }
-
-    return post;
+    return await this.likeRepository.save(like);
   }
 
-  private async findPostLike(postId: string, userId: string): Promise<void> {
+  async unlikeComment(commentId: string, user: User): Promise<void> {
+    await this.commentService.findCommentByIdOrFail(commentId);
+
+    const like = await this.findLikeByEntityAndUserOrFail(
+      'comment',
+      commentId,
+      user.id,
+    );
+    await this.likeRepository.delete(like);
+  }
+
+  async findLikeByEntityAndUser(
+    entityType: 'post' | 'comment',
+    entityId: string,
+    userId: string,
+  ): Promise<Like | null> {
+    return await this.likeRepository.findOne({
+      where: { [entityType]: { id: entityId }, user: { id: userId } },
+    });
+  }
+
+  async findLikeByEntityAndUserOrFail(
+    entityType: 'post' | 'comment',
+    entityId: string,
+    userId: string,
+    error?: string,
+  ): Promise<Like> {
     const like = await this.likeRepository.findOne({
-      where: { post: { id: postId }, user: { id: userId } },
+      where: { [entityType]: { id: entityId }, user: { id: userId } },
     });
 
-    if (like) {
-      throw new ConflictException('Like already exists');
+    if (!like) {
+      throw new NotFoundException(error || 'Like not found');
     }
+
+    return like;
   }
 
   private async validateLikeDoesNotExists(
+    entityType: 'post' | 'comment',
+    entityId: string,
     userId: string,
-    postId?: string,
-    commentId?: string,
   ): Promise<void> {
-    const like = await this.likeRepository.findOne({
-      where: {
-        user: { id: userId },
-        ...(postId && { post: { id: postId } }),
-        ...(commentId && { comment: { id: commentId } }),
-      },
+    const like = await this.likeRepository.exists({
+      where: { [entityType]: { id: entityId }, user: { id: userId } },
     });
 
     if (like) {
