ba3e7c9286e3923719f0eab38d19649c1dfc8322
sebas-sala <sebastian_orko@hotmail.com>
2024-09-29 13:02:38 -0600

    refactor: Update BlockedUsersModule imports and providers
    This commit updates the imports and providers in the BlockedUsersModule. It adds the forwardRef function to import the UsersModule and includes it in the TypeOrmModule.forFeature() method. The QueryRunnerFactory provider is removed from the providers array. This change ensures that the BlockedUsersModule can access the User entity and establishes the necessary dependencies.

--- src/blocked-users/blocked-users.module.ts
+++ src/blocked-users/blocked-users.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
-import { BlockedUsersService } from './blocked-users.service';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BlockedUser } from './entities/blocked-user.entity';
+
+import { UsersModule } from '../users/users.module';
+
+import { BlockedUsersService } from './blocked-users.service';
+
 import { User } from '../users/entities/user.entity';
-import { QueryRunnerFactory } from '../common/factories/query-runner.factory';
+import { BlockedUser } from './entities/blocked-user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BlockedUser, User])],
-  providers: [BlockedUsersService, QueryRunnerFactory],
+  imports: [
+    TypeOrmModule.forFeature([BlockedUser, User]),
+    forwardRef(() => UsersModule),
+  ],
+  providers: [BlockedUsersService],
   exports: [BlockedUsersService],
 })
 export class BlockedUsersModule {}
