94091d7f2987da0a936f0fd1fb4bc0270fb416ab
sebas-sala <sebastian_orko@hotmail.com>
2024-09-29 10:52:15 -0600

    refactor: Update current user decorators to return the entire user object
    The current user decorators in `current-user.decorator.ts` and `ws-current-user.decorator.ts` have been updated to return the entire user object instead of just the userId. This change allows for more flexibility and access to additional user properties in the application.

--- src/common/decorators/ws-current-user.decorator.ts
+++ src/common/decorators/ws-current-user.decorator.ts
@@ -1,9 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const CurrentUser = createParamDecorator(
+export const WsCurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-
+    const request = ctx.switchToWs().getData();
     return request.user;
   },
 );
