25d2d0c8b53c391441594b5ef1fdbb216482310c
sebas-sala <sebastian_orko@hotmail.com>
2024-10-12 01:04:30 -0600

    refactor: Update WsExceptionFilter to include success field in response

--- src/common/filters/ws-exception.filter.ts
+++ src/common/filters/ws-exception.filter.ts
@@ -1,36 +1,19 @@
-import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
-import { WsException } from '@nestjs/websockets';
+import { Catch, ArgumentsHost } from '@nestjs/common';
+import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
+import { Socket } from 'socket.io-client';
 
 @Catch(WsException)
-export class WsExceptionFilter implements ExceptionFilter {
+export class WsExceptionFilter extends BaseWsExceptionFilter {
   catch(exception: WsException, host: ArgumentsHost) {
-    const wsContext = host.switchToWs();
-    const client = wsContext.getClient();
+    const client: Socket = host.switchToWs().getClient<Socket>();
 
-    const errorResponse = exception.getError() as {
-      status?: number;
-      message?: string;
-      name?: string;
-      response?: {
-        message?: string;
-        statusCode?: number;
-        error?: string;
-      };
-    };
-
-    const statusCode = errorResponse.status || 500;
-    const error = errorResponse.name || 'Internal Server Error';
-    const message = errorResponse.message || exception.message;
-
-    const response = {
-      statusCode,
-      error,
-      message,
+    const errorResponse = {
+      success: false,
+      message: exception.message,
+      data: exception.getError(),
       timestamp: new Date().toISOString(),
     };
 
-    console.log('WsExceptionFilter', response);
-
-    client.send(response);
+    client.emit('error', errorResponse);
   }
 }
