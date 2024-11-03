c7f56045b30a6831e7d8c4ac36c4463baf63efdf
sebas-sala <sebastian_orko@hotmail.com>
2024-11-03 01:18:43 -0600

    feat: Enhance JwtAuthGuard to validate session and decode JWT token

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,5 +1,49 @@
-import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  type ExecutionContext,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor() {
+    super();
+  }
+
+  private readonly logger = new Logger(JwtAuthGuard.name);
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const session = request.cookies?.['__session'];
+
+    if (!session) {
+      throw new UnauthorizedException();
+    }
+
+    const jwtSession = session.toString().split('.')[0];
+
+    if (!jwtSession) {
+      throw new UnauthorizedException();
+    }
+
+    let decodedSession: Record<string, any>;
+
+    try {
+      decodedSession = jwtDecode(jwtSession, { header: true });
+    } catch (error) {
+      this.logger.error('Failed to decode session', error);
+      throw new UnauthorizedException('Invalid session token');
+    }
+
+    const token = decodedSession.token;
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    request.headers.authorization = `Bearer ${token}`;
+    return super.canActivate(context);
+  }
+}
