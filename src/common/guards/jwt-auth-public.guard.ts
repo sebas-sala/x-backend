ff107726d16f59532ad169be1e3b7deccc40200e
sebas-sala <sebastian_orko@hotmail.com>
2024-11-05 22:20:56 -0600

    refactor: Enhance JWT authentication guards to improve session handling and token extraction

--- src/common/guards/jwt-auth-public.guard.ts
+++ src/common/guards/jwt-auth-public.guard.ts
@@ -1,20 +1,46 @@
+import { toBoolean } from '@/src/utils/boolean.util';
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable()
 export class JwtAuthPublicGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
+    const session = request.cookies?.['__session'];
 
-    const authToken = request.cookies['__session'];
+    if (!session) {
+      const token = request.cookies?.['token'];
 
-    if (!authToken && !request.headers.authorization) {
+      if (token && toBoolean(token)) {
+        request.headers.authorization = `Bearer ${token}`;
+        return super.canActivate(context);
+      }
+
       return true;
     }
 
-    if (authToken) {
-      request.headers.authorization = `Bearer ${authToken}`;
+    const jwtSession = session.toString().split('.')[0];
+
+    if (!jwtSession) {
+      return true;
     }
+
+    let decodedSession: Record<string, any>;
+
+    try {
+      decodedSession = jwtDecode(jwtSession, { header: true });
+    } catch {
+      return true;
+    }
+
+    const token = decodedSession.token;
+
+    if (!token) {
+      return true;
+    }
+
+    request.headers.authorization = `Bearer ${token}`;
 
     return super.canActivate(context);
   }
--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -9,16 +9,19 @@ import { jwtDecode } from 'jwt-decode';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor() {
-    super();
-  }
-
   private readonly logger = new Logger(JwtAuthGuard.name);
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const session = request.cookies?.['__session'];
 
     if (!session) {
+      const token = request.cookies?.['token'];
+
+      if (token) {
+        request.headers.authorization = `Bearer ${token}`;
+        return super.canActivate(context);
+      }
+
       throw new UnauthorizedException();
     }
 
